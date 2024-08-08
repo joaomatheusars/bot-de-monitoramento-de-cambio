2d7af54ea75d5bd39082c24a470858ab8cb44785
João Matheus <joaomatheusars@gmail.com>
2024-08-08 17:39:46 -0300

    feat: add first config

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from interface.interface import Interface
+import os
+import requests
 
 def first_config():
-    pass
+    if not os.path.isdir(f'{os.getcwd()}/img'):
+        os.mkdir('img')
+        url = 'https://raw.githubusercontent.com/joaomatheusars/bot-de-monitoramento-de-cambio/main/img/dollar-symbol.ico'
+        ico = requests.get(url).content
+        with open(f'{os.getcwd()}/img/dollar-symbol.ico', 'wb') as file:
+            file.write(ico)
+        
 
 def main():
     app = Interface()
     app.mainloop()
-	
  
 if __name__ == "__main__":
+	first_config()
 	main()
